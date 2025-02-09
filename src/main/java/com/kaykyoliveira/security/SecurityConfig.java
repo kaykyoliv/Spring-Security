19dee13eb1be813de39bcd50086003a46300e401
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2025-02-09 11:59:34 -0300

    getting h2 database to work

--- src/main/java/com/kaykyoliveira/security/SecurityConfig.java
+++ src/main/java/com/kaykyoliveira/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.kaykyoliveira.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,6 +29,15 @@ public class SecurityConfig {
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    @Order(1)
+    SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception{
+        http.securityMatcher(PathRequest.toH2Console()).csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
         return http.build();
     }
 
