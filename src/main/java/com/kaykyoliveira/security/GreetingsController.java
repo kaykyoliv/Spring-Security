e1bb6bb48a74a4a7dfdc2cfdffd6c696af83d6e5
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2025-02-09 11:36:53 -0300

    Role based authentication

--- src/main/java/com/kaykyoliveira/security/GreetingsController.java
+++ src/main/java/com/kaykyoliveira/security/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.kaykyoliveira.security;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,18 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint(){
+        return "Hello, User!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint(){
+        return "Hello, Admin!";
     }
 
 
