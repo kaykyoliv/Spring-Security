c3dc9782556bc7206674eace2394810b4d645c66
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2025-02-09 19:35:17 -0300

    Implementing JwtUtils

--- src/main/java/com/kaykyoliveira/security/SecurityConfig.java
+++ src/main/java/com/kaykyoliveira/security/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -51,12 +53,12 @@ public class SecurityConfig {
     public UserDetailsService userDetailsService(){
 
         UserDetails user1 = User.withUsername("user1")
-                .password("{noop}user123")
+                .password(passwordEncoder().encode("user123"))
                 .roles("USER")
                 .build();
 
         UserDetails admin = User.withUsername("admin")
-                .password("{noop}admin123")
+                .password(passwordEncoder().encode("admin123"))
                 .roles("ADMIN")
                 .build();
 
@@ -67,5 +69,10 @@ public class SecurityConfig {
         userDetailsManager.createUser(admin);
 
         return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
